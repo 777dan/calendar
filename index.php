7b836d3c7fe6b663985b6c585dcf7503453fc597
777dan <danya.nikitin7@gmail.com>
2022-11-30 19:57:20 +0200

    add start next month

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $year = date("Y");
 echo "<table>";
 echo "<tr><th>Понедельник</th><th>Вторник</th><th>Среда</th><th>Четверг</th><th>Пятница</th><th>Суббота</th><th>Воскресенье</th></tr>";
 $count = 0;
-for ($i = 0; $i <= date("t"); $i++) {
+for ($i = 0; $i < 35; $i++) {
     if ($count == 7) $count = 0;
     if ($count == 0)echo "<tr>";
     if ($count == 7)echo "<script>document.getElementBy</script>";
